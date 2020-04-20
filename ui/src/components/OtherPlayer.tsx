4db23b010aae6baf128c0805a7f4fad99f9952c6
Alexander Ivkin <ivkin@almworks.com>
2020-04-19 20:34:45 -0400

    Selection state

--- ui/src/components/OtherPlayer.tsx
+++ ui/src/components/OtherPlayer.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import {Score} from './Score';
 import {Player} from '../model/Player';
 import {Card} from './Card';
+import _ = require('lodash');
 
 export interface PlayerProps {
   player: Player;
@@ -13,9 +14,10 @@ export interface PlayerProps {
 export const OtherPlayer = (props: PlayerProps) => {
   const disabledClass = props.selectable ? '' : 'disabled';
   const selectedClass = props.selected ? 'player-selected' : '';
+  const select = props.selectable ? props.onClick : _.noop;
   return (
     <div className='player-wrapper'>
-      <div className={`player ${selectedClass} ${disabledClass}`} onClick={() => props.onClick(props.player)}>
+      <div className={`player ${selectedClass} ${disabledClass}`} onClick={() => select(props.player)}>
         <h3>{props.player?.name || 'Unknown'}</h3>
         <Card/>
       </div>
