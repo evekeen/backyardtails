2a496b2169faf176bcc79935c1c718552d27a0e4
Alexander Ivkin <a@ivkin.dev>
2020-04-25 20:25:23 -0400

    Fixed prod url configuration

--- ui/src/store.ts
+++ ui/src/store.ts
@@ -3,7 +3,8 @@ import loggerMiddleware from './middleware/logger'
 import reducers from './reducers'
 import {WsClient} from './WsClient';
 
-const url = process.env.WS_URL || 'ws://localhost:8999';
+const prod = process.env.NODE_ENV === 'production';
+const url = prod ? 'ws://loveletter-dev.us-west-2.elasticbeanstalk.com:8999' : 'ws://localhost:8999';
 
 const wsClient = new WsClient(url);
 
@@ -15,7 +16,7 @@ export default function configureAppStore(preloadedState: any) {
     enhancers: []
   });
 
-  if (process.env.NODE_ENV !== 'production' && module.hot) {
+  if (!prod && module.hot) {
     module.hot.accept('./reducers', () => store.replaceReducer(reducers));
   }
 
