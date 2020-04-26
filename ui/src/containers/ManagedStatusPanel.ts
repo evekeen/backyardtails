06c37a3280364f6120d409e1d42b81de07bd41ab
Alexander Ivkin <a@ivkin.dev>
2020-04-26 10:07:42 -0400

    Better toasts

--- ui/src/containers/ManagedStatusPanel.ts
+++ ui/src/containers/ManagedStatusPanel.ts
@@ -1,9 +1,12 @@
 import {connect} from 'react-redux';
 import {AppState} from '../components/App';
 import {StatusPanel} from '../components/StatusPanel';
+import {closeMessage} from '../reducers/status';
 
 const mapStateToProps = (state: AppState) => {
   return state.status;
 }
 
-export default connect(mapStateToProps)(StatusPanel);
+const mapDispatchToProps = {closeMessage}
+
+export default connect(mapStateToProps, mapDispatchToProps)(StatusPanel);
